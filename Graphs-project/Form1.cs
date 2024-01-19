142f6d69a31d5bb5b487aecc0b3625ca22826899
Konrad Niedzielski <konrad.niedzielski03@gmail.com>
2024-01-19 23:59:14 +0100

    Implemented connecting edges.

--- Graphs-project/Form1.cs
+++ Graphs-project/Form1.cs
@@ -16,6 +16,7 @@ namespace Graphs_project
     Bitmap bitmap;
     DrawingKit drawingKit;
     List<Edge> edges;
+    Point mousePosition;
 
     public GraphsMainForm()
     {
@@ -24,21 +25,76 @@ namespace Graphs_project
       drawingZone.Image = bitmap;
       drawingKit = new DrawingKit(drawingZone.Image);
       edges = new List<Edge>();
+      mousePosition = new Point();
     }
 
     private void drawingZone_MouseDown(object sender, MouseEventArgs e)
     {
       if (e.Button == MouseButtons.Left)
       {
+        if (!checkColision(e.Location)) return;
+
         edges.Add(new Edge(e.X, e.Y));
         drawingKit.draw(edges);
         drawingZone.Refresh();
+      }
+
+      if (e.Button == MouseButtons.Right)
+      {
+        mousePosition.X = e.X;
+        mousePosition.Y = e.Y;
       }
     }
 
     private void drawingZone_MouseUp(object sender, MouseEventArgs e)
     {
+      if (e.Button == MouseButtons.Right)
+      {
+        Edge firstEdge = getNearestEdge(mousePosition);
+        Edge targetEdge = getNearestEdge(e.Location);
+        if (edgeNotFound(firstEdge) || edgeNotFound(targetEdge)) return;
 
+        firstEdge.Neighbours.Add(targetEdge);
+        targetEdge.Neighbours.Add(firstEdge);
+        drawingKit.draw(edges);
+        drawingZone.Refresh();
+      }
+    }
+
+    private Edge getNearestEdge(Point position)
+    {
+      foreach(Edge edge in edges)
+      {
+        if (distance(position, edge.Position) < drawingKit.radius) return edge;
+      }
+
+      return null;
+    }
+
+    private bool edgeNotFound(Edge edge)
+    {
+      return edge == null ? true : false;
+    }
+
+    private bool checkColision(Point mousePosition)
+    {
+      if (mousePosition.X < 20 ||
+         mousePosition.X > 480 ||
+         mousePosition.Y < 20 ||
+         mousePosition.Y > 480
+        ) return false;
+
+      foreach (Edge edge in edges)
+      {
+        if (distance(mousePosition, edge.Position) < drawingKit.radius * 2.5) return false;
+      }
+
+      return true;
+    }
+
+    private int distance(Point p1, Point p2)
+    {
+      return (int)Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + (int)Math.Pow(p2.Y - p1.Y, 2));
     }
   }
 }
